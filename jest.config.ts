d9761f51ab83afdcea27ea7fd003ef9102be722c
César Lowe <lowinho@yahoo.com.br>
2024-01-10 19:47:42 -0300

    check coverage 80%

--- jest.config.ts
+++ jest.config.ts
@@ -15,10 +15,10 @@ export default {
   },
   coverageThreshold: {
     "global": {
-      "branches": 40,
-      "functions": 40,
-      "lines": 40,
-      "statements": 40
+      "branches": 80,
+      "functions": 80,
+      "lines": 80,
+      "statements": 80
     }
   },
   setupFiles: ["dotenv/config"],
